8d8abdc904e5ae936c140e32ce3396901c1c2d88
Luke Chen <luke.chen.2010@gmail.com>
2020-09-07 20:02:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -36,5 +36,8 @@ function render() {
   }
 };
 
+let store = createStore(reducer)
+store.dispatch({ type: '@@INIT' });
+
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
